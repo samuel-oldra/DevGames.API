8e8d401ddf0dbbcae6c2ceb2e3d82f81025423c7
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-10-05 00:00:00 -0300

    Adicionados controllers boards e posts

---
+++ DevGames/DevGames.API/Controllers/BoardsController.cs
@@ -0,0 +1,47 @@
+﻿using DevGames.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevGames.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardsController : ControllerBase
+    {
+        // GET: api/boards
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        // GET: api/boards/1
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // NotFound()
+
+            return Ok();
+        }
+
+        // POST: api/boards
+        [HttpPost]
+        public IActionResult Post(AddBoardInputModel model)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = model.Id }, model);
+        }
+
+        // PUT: api/boards/1
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UpdateBoardInputModel model)
+        {
+            return NoContent();
+        }
+
+        // DELETE: api/boards/1
+        [HttpDelete("id")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
