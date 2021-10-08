d14ae64bdad7ce892aab589af69b0aa784caaa3e
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-10-08 00:00:00 -0300

    Criando banco de dados em memória

---
+++ DevGames/DevGames.API/Persistence/DevGamesContext.cs
@@ -0,0 +1,14 @@
+﻿using DevGames.API.Entities;
+
+namespace DevGames.API.Persistence
+{
+    public class DevGamesContext
+    {
+        public DevGamesContext()
+        {
+            Boards = new List<Board>();
+        }
+
+        public List<Board> Boards { get; private set; }
+    }
+}
