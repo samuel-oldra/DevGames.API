ddd52297ba0c7eea87fc9620294db8cf22324e2f
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-10-16 00:00:00 -0300

    Alteradas Entities para uso com banco de dados

--- DevGames/DevGames.API/Entities/Comment.cs
+++ DevGames/DevGames.API/Entities/Comment.cs
@@ -2,18 +2,21 @@
 {
     public class Comment
     {
-        public Comment(string title, string description, string user)
+        public Comment(string title, string description, string user, int postId)
         {
             Title = title;
             Description = description;
             User = user;
+            PostId = postId;
 
             CreatedAt = DateTime.Now;
         }
 
+        public int Id { get; private set; }
         public string Title { get; private set; }
         public string Description { get; private set; }
         public string User { get; private set; }
         public DateTime CreatedAt { get; private set; }
+        public int PostId { get; private set; }
     }
 }
--- DevGames/DevGames.API/Entities/Post.cs
+++ DevGames/DevGames.API/Entities/Post.cs
@@ -2,11 +2,11 @@
 {
     public class Post
     {
-        public Post(int id, string title, string description)
+        public Post(string title, string description, int boardId)
         {
-            Id = id;
             Title = title;
             Description = description;
+            BoardId = boardId;
 
             CreatedAt = DateTime.Now;
             Comments = new List<Comment>();
@@ -17,6 +17,7 @@
         public string Description { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public List<Comment> Comments { get; private set; }
+        public int BoardId { get; private set; }
 
         public void AddCommet(Comment comment)
         {
