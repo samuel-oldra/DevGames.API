23feefddb284f1a1296a86140ddced03769f47ab
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-02-01 13:56:58 -0300

    Swagger: Adicionando informações de contato

--- DevGames/DevGames.API/Program.cs
+++ DevGames/DevGames.API/Program.cs
@@ -2,6 +2,8 @@ using DevGames.API.Mappers;
 using DevGames.API.Persistence;
 using DevGames.API.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,7 +27,24 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "DevGames API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Samuel B. Oldra",
+            Email = "samuel.oldra@gmail.com",
+            Url = new Uri("https://github.com/samuel-oldra")
+        }
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    o.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
